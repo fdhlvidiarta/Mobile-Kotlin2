56e70d83cd5ef1705404a821096324ed526c3d0d
fadhil <fadhilv05@gmail.com>
2024-03-04 23:23:23 +0700

    Tugas Codelab Aquarium

---
+++ app/src/main/java/com/fadhil/mobile_kotlin/Aquarium.kt
@@ -0,0 +1,48 @@
+package com.fadhil.mobile_kotlin
+
+import java.util.Random   // required import
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else  -> false
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day =="Sunday"
+fun main(args: Array<String>) {
+    feedTheFish()
+    swim("turtle-like")
+}
